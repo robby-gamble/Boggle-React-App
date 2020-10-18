69c0a6325d2856c47e468a5407e9889b77bb7b61
Robby Gamble <robbygamble@Robbys-MacBook-Pro.local>
2020-10-18 16:40:52 -0400

    Added base code to GuessInput and ToggleState.

--- boggle-solver/src/componenets/jsFiles/GuessInput.js
+++ boggle-solver/src/componenets/jsFiles/GuessInput.js
@@ -1,3 +1,17 @@
 import React, { useState } from 'react';
 import TextField from "@material-ui/core/TextField";
 import './GuessInput.css';
+
+function GuessInput() {
+
+    return (
+      <div className="Guess-input">
+        <div>
+          {labelText}
+        </div>
+        <TextField onKeyPress= ""/>
+      </div>
+    );
+  }
+  
+  export default GuessInput;
--- boggle-solver/src/componenets/jsFiles/ToggleState.js
+++ boggle-solver/src/componenets/jsFiles/ToggleState.js
@@ -6,3 +6,39 @@ import FormHelperText from '@material-ui/core/FormHelperText';
 import Select from '@material-ui/core/Select';
 import FormControl from '@material-ui/core/FormControl';
 import './ToggleGameState.css';
+
+function ToggleGameState() {
+
+    return (
+      <div className="Toggle-game-state">
+        <Button variant="outlined">
+          {buttonText}
+        </Button>
+  
+          <div className="Input-select-size">
+          <FormControl >
+         
+          <Select
+            labelId="sizelabel"
+            id="sizemenu"
+          
+         
+            onChange={handleChange}
+          >
+            <MenuItem value={3}>3</MenuItem>
+            <MenuItem value={4}>4</MenuItem>
+            <MenuItem value={5}>5</MenuItem>
+            <MenuItem value={6}>6</MenuItem>
+            <MenuItem value={7}>7</MenuItem>
+            <MenuItem value={8}>8</MenuItem>
+            <MenuItem value={9}>9</MenuItem>
+            <MenuItem value={10}>10</MenuItem>
+          </Select>
+           <FormHelperText>Set Grid Size</FormHelperText>
+          </FormControl>
+         </div>
+      </div>
+    );
+  }
+  
+  export default ToggleGameState;
